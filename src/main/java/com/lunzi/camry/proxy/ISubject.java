95f872f767bf26286ec0a9bca8a195f8b8207a07
wushengtao <1104119135@qq.com>
2019-02-27 09:51:05 +0800

    [MJB-202]代理模式

--- src/main/java/com/lunzi/camry/proxy/ISubject.java
+++
@@ -1,9 +0,0 @@
-package com.lunzi.camry.proxy;
-
-/**
- * 被代理的接口
- * Created by lunzi on 2018/7/18 上午7:57
- */
-public interface ISubject {
-    void print();
-}
---
+++ src/main/java/com/lunzi/camry/proxy/Man.java
@@ -0,0 +1,7 @@
+package com.lunzi.camry.proxy;
+
+/**
+ * Created by lunzi on 2019/2/26 4:05 PM
+ */
+public class Man {
+}
