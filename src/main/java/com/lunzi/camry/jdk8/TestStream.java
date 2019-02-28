1c48c615a8445af5f28430ba5d6a0922a22bee49
wushengtao <LunZ>
2019-02-28 09:44:42 +0800

    jdk8 stream

--- src/main/java/com/lunzi/camry/jdk8/TestStream.java
+++ src/main/java/com/lunzi/camry/jdk8/TestStream.java
@@ -1,41 +1,75 @@
 package com.lunzi.camry.jdk8;
 
-import com.google.common.collect.Lists;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
+ * 流相关的操作
+ * <p>
  * Created by lunzi on 2018/11/16 10:14 AM
  */
 public class TestStream {
-    private int size;
+    //创建流
+    public static void createStream() {
+        Stream<Integer> streamOf = Stream.of(1, 2, 3);//of：由...组成
+        Stream<Double> streamGen = Stream.generate(Math::random);
+        Stream<Integer> streamIter = Stream.iterate(0, n -> n + 2);//无限流
 
-    public static void main(String args[]){
-        testStream();
     }
-    public static void testStream(){
-        List<Integer> list= Lists.newArrayList();
-        list.add(5);
-        list.add(1);
-        list.add(3);
-        list.add(2);
-        list.add(4);
-        list=list.stream().filter(x->x>1)
-                .sorted(Comparator.comparing(Integer::intValue).reversed())
-                .collect(Collectors.toList());
-        System.out.println(list);
-        Long count= list.stream().filter(x->x>1)
-                .sorted(Comparator.comparing(Integer::intValue).reversed())
-                .count();
-        System.out.println(count);
-        String [] words={"hello","word"};
-        List l=Arrays.stream(words).map(x->x.split(""))
-                .flatMap(Arrays::stream)
-                .distinct()
-                .collect(Collectors.toList());
-        System.out.println(l);
+
+    //去重，排序，截断，计算个数
+    public static void test1() {
+        Stream<Integer> streams = Stream.of(1, 3, 5, 7, 2, 4, 7, 8, 9, 2);
+        streams.distinct();//去重
+        streams.sorted();//排序
+        streams.limit(1);//截断
+        streams.count();//计算个数
+
+    }
+
+    //latMap、flatMapToInt、flatMapToLong、flatMapToDouble
+    //allMatch、noneMatch、anyMatch
+    //findFirst、findAny、filter
+    //skip、peek、collect
+    //reduce
+    public static void test2() {
+        Stream<Integer> streams = Stream.of(1, 2, 3, 4);
+        streams.map(x->x*2);//map 映射
+        streams.mapToLong(x->x);
+        streams.mapToInt(x->x);
+        streams.mapToDouble(x->x);
+        streams.skip(1);
+    }
+
+
+    public static void test3(){
+        Stream<Integer> streams = Stream.of(1, 2, 3, 4);
+        streams.flatMap(x->{
+            return streams.skip(1);
+        });
+    }
+
+    public static void test4(){
+
+    }
+
+    public static void testPara() {
+        IntStream.range(1, 10).parallel().forEach((num) -> {
+            try {
+                Thread.sleep(2000);
+                if (num % 2 == 0) {
+                    System.out.println("偶数");
+                } else {
+                    System.out.println("质数");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
     }
 }
